ea60aff4d0d3fda4227a775ba7e9fa7a27f6570b
Tapan <tapanmanu2000@gmail.com>
2020-10-11 17:19:03 +0530

    sample chart added

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import "./index.css";
-import Footer from "./components/Footer";
+import Report from "./components/Report";
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render( <Footer /> , document.getElementById('root'));
+ReactDOM.render( <Report /> , document.getElementById('root'));
 
 registerServiceWorker();
