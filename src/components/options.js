1db5251acdf0c1527ab90bc9169dd8be393b9f44
VI-Suji <sujithvi08@gmail.com>
2020-10-16 17:01:55 +0530

    options

--- src/components/options.js
+++ src/components/options.js
@@ -1,20 +1,24 @@
 import React from "react";
+import { Component } from "react";
 class QuestionCard extends Component {
   render() {
-    return <h1>Questions</h1>;
+    return (
+      <div>
+        <h1>Questions</h1>
+        <Form>
+          <Row>
+            <Col>
+              <Form.Control placeholder="First name" />
+            </Col>
+            <Col>
+              <Form.Control placeholder="Last name" />
+            </Col>
+          </Row>
+        </Form>
+        ;
+      </div>
+    );
   }
-}
-{
-  /* <Form>
-  <Row>
-    <Col>
-      <Form.Control placeholder="First name" />
-    </Col>
-    <Col>
-      <Form.Control placeholder="Last name" />
-    </Col>
-  </Row>
-</Form>; */
 }
 
 export default QuestionCard;
