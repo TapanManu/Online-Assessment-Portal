e6cffabdbd4a60b968088754c8997e3073c23ae4
VI-Suji <sujithvi08@gmail.com>
2020-10-16 16:55:51 +0530

    options

---
+++ src/components/options.js
@@ -0,0 +1,20 @@
+import React from "react";
+class QuestionCard extends Component {
+  render() {
+    return <h1>Questions</h1>;
+  }
+}
+{
+  /* <Form>
+  <Row>
+    <Col>
+      <Form.Control placeholder="First name" />
+    </Col>
+    <Col>
+      <Form.Control placeholder="Last name" />
+    </Col>
+  </Row>
+</Form>; */
+}
+
+export default QuestionCard;
--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import "./index.css";
 
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from "./registerServiceWorker";
 // import Signup from "./components/signup";
-import Instruction from "./components/instruction";
-
-ReactDOM.render( <Instruction /> , document.getElementById('root'));
+// import Instruction from "./components/instruction";
+import QuestionCard from "./components/options";
+ReactDOM.render(<QuestionCard />, document.getElementById("root"));
 
 registerServiceWorker();
